f505fea26810fa4df559690f160d3cc67a758a29
a01613555-cmd <a01613555@tec.mx>
2025-12-02 09:40:17 -0600

    Datos.py

--- Datos.py
+++ Datos.py
@@ -6,7 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 st.write(''' # Predicción de gastos ''')
-st.image("imagen.jpg", caption="El Titanic navegaba desde Southampton, Inglaterra, hasta Nueva York en Estados Unidos.")
+st.image("imagen.webp", caption="El Titanic navegaba desde Southampton, Inglaterra, hasta Nueva York en Estados Unidos.")
 
 st.header('Datos de evaluación')
 
